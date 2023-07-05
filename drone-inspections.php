2b942a9e7ebb2053e1b5900be68fabcc18643af1
nandhuaries <nandhu.s@ariesesolutions.com>
2023-07-05 18:25:47 +0530

    added drone inspection page and added arrow in navbar

--- drone-inspections.php
+++ drone-inspections.php
@@ -1,3 +1,129 @@
 <?php include("header.php"); ?>
+<div class="hero px-container bg-blue">
+    <div class="container">
+        <div class="hero-main">
+            <div class="hero-subheading animate-text" data-splitting>
+                SERVICES
+            </div>
+            <h1 class="hero-heading hero-heading-sm animate-text" data-splitting>
+                DRONE INSPECTIONS
+            </h1>
+        </div>
+    </div>
+    <a class="hero-scroll-btn" href="#post-104"></a>
+    <div class="hero-bg" style="background-image: url('our-images/services/service3.jpg');"></div>
+</div>
+<!-- content section -->
+<div class="split px-container bg-body" id="post-104">
+    <div class="split-container">
+        <div class="split-image" style="
+              background-image: url('our-images/services/drone--.PNG');
+            "></div>
+        <div class="split-content">
+            <div class="split-content-container">
+                <div class="split-content-heading">
+                    <h2 class="heading-lg animate-text" data-splitting>
+                        DRONE INSPECTIONS
+                    </h2>
+                    <!-- try -->
+                    <div class="richtext richtext-fade-in">
+                        <p>
+                            Unmanned aerial vehicles (UAVs), or drones, are finding applications in an increasing number
+                            of industries. Intertek has added the use of drone and robotics to create an inspection
+                            program capable of acquiring data that would eliminate working in hazardous locations
+                            (flare tip, at height, rope access, confined space, offshore under platform, etc.) and
+                            costly additions such as scaffolds, manlifts, downtime, and personnel hours.
+                        </p>
+                        <p>
+                            Thanks to the data collected by drones, key energy stakeholders now have more actionable
+                            insights into energy-generating equipment, projects, and operations, empowering them to
+                            make smarter decisions.
+                        </p>
+                    </div>
+                    <!-- end try -->
+                </div>
+                <div class="split-content-body">
+                    <div class="richtext richtext-fade-in">
+                        <!-- <p>
+                            Unmanned aerial vehicles (UAVs), or drones, are finding applications in an increasing number
+                            of industries. Intertek has added the use of drone and robotics to create an inspection
+                            program capable of acquiring data that would eliminate working in hazardous locations
+                            (flare tip, at height, rope access, confined space, offshore under platform, etc.) and
+                            costly additions such as scaffolds, manlifts, downtime, and personnel hours.
+                        </p>
+                        <p>
+                            Thanks to the data collected by drones, key energy stakeholders now have more actionable
+                            insights into energy-generating equipment, projects, and operations, empowering them to
+                            make smarter decisions.
+                        </p> -->
+                        <div class="contact-boxes-item fade-in-item">
+                            <div class="contact-box">
+                                <h2 class="contact-box-heading">The use of drones offers advantages such as:</h2>
+                                <ul class="contact-details-links">
+                                    <li>
+                                        Quick Overview And Evaluation Of The Condition
+                                    </li>
+                                    <li>
+                                        Sharp And Detailed Photographs Of Defects
+                                    </li>
+                                    <li>
+                                        Preventive Maintenance Planning And Optimised Production
+                                    </li>
+                                    <li>
+                                        Access To Hard To Reach Areas
+                                    </li>
+                                    <li>
+                                        Reduced Downtime
+                                    </li>
+                                    <li>
 
+                                        High Level Of Safety
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <!-- Scope of inspections -->
+
+                        <div class="contact-boxes-item fade-in-item">
+                            <div class="contact-box">
+                                <h2 class="contact-box-heading">Scope of inspections</h2>
+                                <ul class="contact-details-links">
+                                    <li>
+                                        Over Head Transmission Line Drone IRT
+                                    </li>
+                                    <li>
+                                        Confined Space Inspection
+                                    </li>
+                                    <li>
+                                        Flare Stack And Chimney
+                                    </li>
+                                    <li>
+                                        Solar Panels
+                                    </li>
+                                    <li>
+                                        Pipeline Inspections
+                                    </li>
+                                    <li>
+                                        Telecommunication Towers
+                                    </li>
+                                    <li>
+                                        Submerged Tank Inspections
+                                    </li>
+                                    <li>
+                                        Bridge Inspections
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <!-- end Scope of inspections -->
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- end content section -->
 <?php include("footer.php"); ?>
--- index.php
+++ index.php
@@ -1,91 +1,6 @@
 <?php include("header.php"); ?>
 
-<body class="home page-template page-template-front-page page-template-front-page-php page page-id-7 theme-helideck_theme woocommerce-no-js ">
 
-    <!-- Google Tag Manager (noscript) -->
-    <!-- <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M9DF2SR" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> -->
-    <!-- End Google Tag Manager (noscript) -->
-
-    <!-- old nav code -->
-
-    <!-- <div class="animsition">
-        <div class="site-menu">
-            <nav>
-                <ul id="menu-responsive-menu" class="site-menu-nav-primary">
-                    <li id="menu-item-236" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-236"><a title="About" href="index">Home</a></li>
-                    <li id="menu-item-237" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-237"><a title="Sectors" href="index#scrollTarget">About</a></li>
-                    <li id="menu-item-237" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-237"><a title="Sectors" href="index#services">service</a></li>
-                    <li id="menu-item-237" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-237"><a title="Sectors" href="index#contact">contact</a></li>
-                </ul>
-            </nav>
-        </div>
-    </div> -->
-
-    <!-- end old nav code -->
-
-    <!-- form chatgpt -->
-
-    <div class="animsition">
-        <div class="site-menu">
-            <nav>
-                <ul id="menu-responsive-menu" class="site-menu-nav-primary">
-                    <li id="menu-item-236" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-236 site-menu-toggle"><a title="Home" href="./">Home</a></li>
-                    <li id="menu-item-237" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-237 site-menu-toggle"><a title="About" href="#scrollTarget">About</a></li>
-                    <li id="menu-item-238" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-238 site-menu-toggle"><a title="Service" href="#services">Service</a></li>
-                    <li id="menu-item-239" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-239 site-menu-toggle"><a title="Contact" href="#contact">Contact</a></li>
-                </ul>
-            </nav>
-        </div>
-    </div>
-
-
-    <!-- end form chatgpt -->
-
-    <header class="site-header">
-        <div class="site-header-container px-container">
-            <div class="site-header-logo">
-                <a href="index">
-                    <img src="our-images/logo.png">
-                    <!-- <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 171 60">
-                            <defs>
-                                <clipPath id="a">
-                                    <path d="M0,0H59.875V60H0Z" fill="none" />
-                                </clipPath>
-                            </defs>
-                            <path d="M0,0H59.875V60H0Z" fill="none" />
-                            <g clip-path="url(#a)">
-                                <path d="M7.972,27.889,0,19.871l3.322-3.343L7.97,21.2,29.272,0a29.618,29.618,0,0,1,3.039,3.628L7.972,27.889" transform="translate(21.959 8.923)" fill="#fffffe" />
-                                <path d="M30,60c-.595,0-1.2-.018-1.812-.054V51.719l-1.28,8.125a30.246,30.246,0,0,1-3.272-.517l1.413-8.97L22.258,59a29.5,29.5,0,0,1-3.029-.979l3.054-9.454-4.488,8.861a30.305,30.305,0,0,1-2.72-1.383l4.878-9.631-6.321,8.751c-.447-.292-.879-.59-1.284-.885-.328-.239-.672-.5-1.081-.829l6.827-9.452-8.222,8.27c-.7-.629-1.363-1.29-1.983-1.964l8.833-8.886L6.611,48.805C6.03,48.081,5.5,47.36,5.034,46.661l10.818-7.906L3.934,44.864c-.427-.751-.817-1.507-1.158-2.248L15.56,36.064,1.9,40.527c-.27-.726-.517-1.482-.735-2.247l14.571-4.762L.592,35.931c-.143-.709-.258-1.43-.355-2.2L16.23,31.18H.028c-.031-.772-.036-1.458-.017-2.1H17.076L.216,26.394C.3,25.7.4,25.067.511,24.452l17.146,2.732a12.72,12.72,0,0,0,4.886,13.085,12.477,12.477,0,0,0,7.405,2.424,12.64,12.64,0,0,0,12.61-13.037H59.917c.013.973-.021,1.939-.1,2.875h-.029l.029,0a29.913,29.913,0,0,1-.755,4.6L58.23,37l.8.263a29.971,29.971,0,0,1-1.425,4.3l-1.573-.515L57.5,41.8a30.155,30.155,0,0,1-2.022,3.9l-2.188-1.121,1.985,1.451c-.4.631-.743,1.14-1.08,1.6-.418.577-.891,1.174-1.444,1.824l-2.372-1.733,2.074,2.085a30.053,30.053,0,0,1-2.924,2.9l-3.112-3.129,2.588,3.584a30.006,30.006,0,0,1-3.238,2.319L42.9,51.512l2.215,4.373a29.971,29.971,0,0,1-3.436,1.732L39.09,52.51l1.757,5.438a30.133,30.133,0,0,1-3.534,1.137L35.3,52.851l1.017,6.466a30.368,30.368,0,0,1-3.536.547l-1.147-7.288v7.379C31.091,59.985,30.541,60,30,60ZM17.676,27.1h0L1.161,21.7c.154-.534.339-1.1.565-1.742l16.508,5.4a12.555,12.555,0,0,0-.558,1.743Zm.64-1.954h0L2.855,17.219c.242-.511.5-1.02.769-1.514l15.433,7.909a12.619,12.619,0,0,0-.74,1.526Zm.938-1.831h0L5.237,13.068l.132-.2c.113-.167.229-.339.35-.507.089-.122.183-.243.274-.36.053-.069.106-.137.159-.206L20.158,22.037l-.113.137-.016.019c-.093.112-.189.228-.277.35-.163.224-.308.459-.448.687l-.05.081Zm1.216-1.663h0L8.238,9.346C8.566,9,8.9,8.656,9.244,8.33L21.461,20.619a12.261,12.261,0,0,0-.991,1.028Zm1.479-1.434h0L11.786,6.146c.341-.26.693-.516,1.048-.763L22.986,19.44a12.429,12.429,0,0,0-1.037.773Zm15.6-.341h0c-.028-.021-.054-.044-.08-.066s-.058-.049-.088-.072-.041-.027-.062-.04-.041-.026-.062-.04L47.4,5.61c.029.021.06.042.09.062s.056.038.084.057l.057.044c.021.017.043.034.066.051L37.545,19.872Zm-13.91-.845h0L15.8,3.556c.361-.194.726-.374,1.036-.526l7.83,15.458c-.346.162-.691.343-1.027.539Zm12.28-.2h0c-.156-.085-.3-.161-.441-.23L43.3,3.141l.19.094c.087.042.172.084.257.128L35.915,18.828ZM25.5,18.138h0L20.17,1.651l.082-.029c.292-.1.594-.2.893-.3l5.327,16.491c-.371.107-.681.208-.975.32Zm8.653-.1h0c-.163-.058-.329-.107-.489-.154l-.112-.033L38.873,1.358l.226.07c.124.038.251.077.376.118L34.149,18.034Zm-6.669-.463h0L24.785.457c.316-.055.613-.1.883-.142l2.694,17.111c-.272.035-.56.082-.881.146Zm4.8-.033h0l-.1-.018c-.214-.04-.435-.082-.654-.109L34.22.3c.257.037.5.076.753.118L32.279,17.538Zm-2.749-.209h0V0h.131c.125,0,.251,0,.376,0l.255,0V17.32l-.189,0c-.19,0-.379.006-.572.013Z" transform="translate(-0.044 0)" fill="#e8842d" />
-                            </g>
-                            <path d="M54.064,41.189A20.3,20.3,0,0,1,41.6,36.928a20.637,20.637,0,0,1-7.242-10.76V40.263H23.475V24.521h-12.6V40.263H0V.926H10.877v13.58h12.6V.926H34.353V15.02A20.574,20.574,0,0,1,47.364,1.128,20.378,20.378,0,0,1,68.982,6.5l-7.717,7.012a9.972,9.972,0,0,0-7.2-3.052,10.139,10.139,0,0,0,0,20.277,9.97,9.97,0,0,0,7.2-3.053l5.958,5.413L81.046.926h8.5L106.56,40.263H94.372l-3.119-7.717h-12.1l-3.018,7.717H64.143L65.159,37.9A20.3,20.3,0,0,1,54.064,41.189ZM85.075,16.966,82.793,22.84h4.592Z" transform="translate(64.44 9.405)" fill="#fffffe" />
-                        </svg> -->
-                </a>
-            </div>
-            <div class="site-header-right">
-                <button class="site-menu-toggle">
-                    <div class="menu-icon">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </div>
-                    <div class="close-icon">
-                        <span></span>
-                        <span></span>
-                    </div>
-                </button>
-                <!-- <a class="site-header-btn" href="contact.php">
-                    <span class="material-icons">account_circle</span>
-                    <span>Sign in</span>
-                </a> -->
-                <!--
-                                <a class="site-header-basket" href="https://www.helidecks.org/cart/">
-                    <span class="material-icons">shopping_basket</span>
-                    <span id="cart-items" class="basket-counter">0</span>
-                </a>
-                                -->
-            </div>
-        </div>
-    </header>
 
     <div class="home-hero hero hero-full bg-blue">
         <div class="px-container">
@@ -186,7 +101,7 @@
                     </a>
                 </li>
                 <li>
-                    <a class="sectors-listings-item" href="">
+                    <a class="sectors-listings-item" href="drone-inspections.php">
                         <div class="content">
                             <div class="content-top">
                                 <h3 class="heading-md">Drone Inspections</h3>
